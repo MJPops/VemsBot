aee11d956abb8a832d21b1cf5cbe388def1b0088
MJSasha <sasha.ushankov@gmail.com>
2021-10-26 00:19:00 +0300

    v15

--- Vems_Bot/Program.cs
+++ Vems_Bot/Program.cs
@@ -10,7 +10,7 @@ namespace Vems_Bot
 {
     class Program
     {
-        private static string token { get; set; } = "2065215367:AAHxs51AowRJAqefe3tvV7d5jn5nsC_-xDc";
+        private static string token { get; set; } = "Токен";
         private static TelegramBotClient client;
 
         
